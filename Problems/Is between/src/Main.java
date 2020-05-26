5781a4a1c317268593ffffcd340141e573bb0300
adallauriano <laureano.adalberto@gmail.com>
2020-05-26 07:36:10 +0100

    problem of the day algoritm with hyperskill

---
+++ Problems/Is between/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        if (b < a && b < c || b > a && b > c || b == a && c == a ) {
+            System.out.println("true");
+        }else {
+            System.out.println("false");
+        }
+    }
+}
